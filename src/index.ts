d7f0b1361b933fc05de58fa6dc37f48830a6b633
Lucas Moura <lucasgabrieldemoura.2021@gmail.com>
2023-08-28 20:38:13 -0300

    feat: add express and making a server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express from "express";
+import { config } from "dotenv";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => console.log(`Escutando da porta ${port}!`))
+
+app.get("/", (req, res) => {
+    res.send("Hello World Node+Typescript")
+})
