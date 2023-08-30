df884204ba5b70fd8451915562bf766850c8a7b1
Lucas Moura <lucasgabrieldemoura.2021@gmail.com>
2023-08-30 16:42:40 -0300

    feat: Make some configs to use Postgres in Node

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import express from "express";
-import { GetUsersController } from "./controllers/get-users/get-users";
-import { PostgresGetUsersRepository } from "./repositories/get-user/postgres-get-users";
+import { GetUsersController } from "./controllers/getUsers/getUsers";
+import { PostgresGetUsersRepository } from "./repositories/getUsers/postgresGetUsers";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-
-app.listen(port, () => console.log(`Escutando da porta ${port}!`))
+app.listen(port, () => console.log(`Escutando da porta ${port}!`));
 
 app.get("/users", async (req, res) => {
-    const postgresGetUsersRepository =  new PostgresGetUsersRepository();
-    const getUserController = new GetUsersController(postgresGetUsersRepository);
+  const postgresGetUsersRepository = new PostgresGetUsersRepository();
+  const getUserController = new GetUsersController(postgresGetUsersRepository);
 
-    const response = await getUserController.handle()
+  const response = await getUserController.handle();
 
-    res.send(response.body).status(response.statusCode)
-})
+  res.send(response.body).status(response.statusCode);
+});
